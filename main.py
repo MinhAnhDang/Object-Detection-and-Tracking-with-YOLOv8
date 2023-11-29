50f8046867149ea917518638b46e248ed9ac2529
Minh Anh Đặng <dangminhanh64@gmail.com>
2023-11-29 22:49:44 +0700

    Update tracking

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 import cv2
 from ultralytics import YOLO
+from tracker import Tracker
 import random
 
 detection_threshold = 0.5
+tracker = Tracker()
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
+
 video_path = os.path.join('.', 'data', 'people.mp4')
 cap = cv2.VideoCapture(video_path)
 ret, frame = cap.read()
+
 cap_out_path = os.path.join('.', 'out.mp4')
 cap_out = cv2.VideoWriter(cap_out_path, cv2.VideoWriter_fourcc(*'MP4V'), cap.get(cv2.CAP_PROP_FPS),
                           (frame.shape[1], frame.shape[0]))
 model = YOLO('yolov8n.pt')
-colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
+
 while ret:
     results = model(frame)
     for result in results:
@@ -25,8 +30,14 @@ while ret:
             class_id = int(class_id)
             if score > detection_threshold:
                 detections.append([x1, y1, x2, y2, score])
+            tracker.update(frame, detections)
 
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255,0,0), 3)
+        for track in tracker.tracks:
+            x1, y1, x2, y2 = track.bbox
+            track_id = track.track_id
+
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
+
     cap_out.write(frame)
     # cv2.imshow("frame", frame)
     # cv2.waitKey(25)
